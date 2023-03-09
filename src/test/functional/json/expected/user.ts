9c478daccdc2f099cb26dc1c10654e06fc1bac8d
Roy <pawit2540@hotmail.com>
2023-03-09 15:11:41 +0700

    fix test

--- src/test/functional/json/expected/user.ts
+++ src/test/functional/json/expected/user.ts
@@ -2,11 +2,13 @@ import * as z from "zod"
 import { CompletePost, RelatedPostModel } from "./index"
 
 // Helper schema for JSON fields
-type JsonObject = { [Key in string]?: JsonValue }
-type JsonArray = Array<JsonValue>
-type JsonValue = string | number | boolean | JsonObject | JsonArray | null
-const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()])
-const jsonSchema: z.ZodSchema<JsonValue> = z.lazy(() => z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)]))
+export type JsonObject = { [Key in string]?: JsonValue }
+export type JsonArray = Array<JsonValue>
+export type JsonValue = string | number | boolean | JsonObject | JsonArray | null
+export const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()])
+export const jsonSchema: z.ZodSchema<JsonValue> = z.lazy(() =>
+	z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)])
+)
 
 export const UserModel = z.object({
   id: z.number().int(),
--- src/test/functional/optional/expected/user.ts
+++ src/test/functional/optional/expected/user.ts
@@ -1,20 +1,22 @@
-import * as z from "zod"
-import { CompletePost, RelatedPostModel } from "./index"
+import * as z from 'zod'
+import { CompletePost, RelatedPostModel } from './index'
 
 // Helper schema for JSON fields
-type JsonObject = { [Key in string]?: JsonValue }
-type JsonArray = Array<JsonValue>
-type JsonValue = string | number | boolean | JsonObject | JsonArray | null
-const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()])
-const jsonSchema: z.ZodSchema<JsonValue> = z.lazy(() => z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)]))
+export type JsonObject = { [Key in string]?: JsonValue }
+export type JsonArray = Array<JsonValue>
+export type JsonValue = string | number | boolean | JsonObject | JsonArray | null
+export const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()])
+export const jsonSchema: z.ZodSchema<JsonValue> = z.lazy(() =>
+	z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)])
+)
 
 export const UserModel = z.object({
-  id: z.number().int(),
-  meta: jsonSchema,
+	id: z.number().int(),
+	meta: jsonSchema,
 })
 
 export interface CompleteUser extends z.infer<typeof UserModel> {
-  posts?: CompletePost | null
+	posts?: CompletePost | null
 }
 
 /**
@@ -22,6 +24,8 @@ export interface CompleteUser extends z.infer<typeof UserModel> {
  *
  * NOTE: Lazy required in case of potential circular dependencies within schema
  */
-export const RelatedUserModel: z.ZodSchema<CompleteUser> = z.lazy(() => UserModel.extend({
-  posts: RelatedPostModel.nullish(),
-}))
+export const RelatedUserModel: z.ZodSchema<CompleteUser> = z.lazy(() =>
+	UserModel.extend({
+		posts: RelatedPostModel.nullish(),
+	})
+)
