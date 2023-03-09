5733e725ea27113d2b2bf8a86e63293f8a11e845
Roy <pawit2540@hotmail.com>
2023-03-09 15:16:25 +0700

    fix test 2

--- src/test/functional/imports/expected/spreadsheet.ts
+++ src/test/functional/imports/expected/spreadsheet.ts
@@ -1,26 +1,24 @@
-import * as z from 'zod'
-import { CompletePresentation, RelatedPresentationModel } from './index'
+import * as z from "zod"
+import { CompletePresentation, RelatedPresentationModel } from "./index"
 
 // Helper schema for JSON fields
 export type JsonObject = { [Key in string]?: JsonValue }
 export type JsonArray = Array<JsonValue>
 export type JsonValue = string | number | boolean | JsonObject | JsonArray | null
 export const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()])
-export const jsonSchema: z.ZodSchema<JsonValue> = z.lazy(() =>
-	z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)])
-)
+export const jsonSchema: z.ZodSchema<JsonValue> = z.lazy(() => z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)]))
 
 export const SpreadsheetModel = z.object({
-	id: z.string(),
-	filename: z.string(),
-	author: z.string(),
-	contents: jsonSchema,
-	created: z.date(),
-	updated: z.date(),
+  id: z.string(),
+  filename: z.string(),
+  author: z.string(),
+  contents: jsonSchema,
+  created: z.date(),
+  updated: z.date(),
 })
 
 export interface CompleteSpreadsheet extends z.infer<typeof SpreadsheetModel> {
-	presentations: CompletePresentation[]
+  presentations: CompletePresentation[]
 }
 
 /**
@@ -28,8 +26,6 @@ export interface CompleteSpreadsheet extends z.infer<typeof SpreadsheetModel> {
  *
  * NOTE: Lazy required in case of potential circular dependencies within schema
  */
-export const RelatedSpreadsheetModel: z.ZodSchema<CompleteSpreadsheet> = z.lazy(() =>
-	SpreadsheetModel.extend({
-		presentations: RelatedPresentationModel.array(),
-	})
-)
+export const RelatedSpreadsheetModel: z.ZodSchema<CompleteSpreadsheet> = z.lazy(() => SpreadsheetModel.extend({
+  presentations: RelatedPresentationModel.array(),
+}))
--- src/test/functional/optional/expected/user.ts
+++ src/test/functional/optional/expected/user.ts
@@ -1,22 +1,20 @@
-import * as z from 'zod'
-import { CompletePost, RelatedPostModel } from './index'
+import * as z from "zod"
+import { CompletePost, RelatedPostModel } from "./index"
 
 // Helper schema for JSON fields
 export type JsonObject = { [Key in string]?: JsonValue }
 export type JsonArray = Array<JsonValue>
 export type JsonValue = string | number | boolean | JsonObject | JsonArray | null
 export const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()])
-export const jsonSchema: z.ZodSchema<JsonValue> = z.lazy(() =>
-	z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)])
-)
+export const jsonSchema: z.ZodSchema<JsonValue> = z.lazy(() => z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)]))
 
 export const UserModel = z.object({
-	id: z.number().int(),
-	meta: jsonSchema,
+  id: z.number().int(),
+  meta: jsonSchema,
 })
 
 export interface CompleteUser extends z.infer<typeof UserModel> {
-	posts?: CompletePost | null
+  posts?: CompletePost | null
 }
 
 /**
@@ -24,8 +22,6 @@ export interface CompleteUser extends z.infer<typeof UserModel> {
  *
  * NOTE: Lazy required in case of potential circular dependencies within schema
  */
-export const RelatedUserModel: z.ZodSchema<CompleteUser> = z.lazy(() =>
-	UserModel.extend({
-		posts: RelatedPostModel.nullish(),
-	})
-)
+export const RelatedUserModel: z.ZodSchema<CompleteUser> = z.lazy(() => UserModel.extend({
+  posts: RelatedPostModel.nullish(),
+}))
